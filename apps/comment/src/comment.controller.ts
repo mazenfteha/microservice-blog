8d2bd1d8a7ea1a0a2526f86684359c6412cee139
mazenfteha <mazn34903@gmail.com>
2024-05-22 19:46:46 -0700

    Comment Service : Get comment and delete comment API

--- apps/comment/src/comment.controller.ts
+++ apps/comment/src/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { JwtGuard } from '../../../libs/comman/src/';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -13,5 +13,15 @@ export class CommentController {
   @Post('create')
   createComment(@GetUser('id') userId: number, @Body() createCommentDto: CreateCommentDto) {
     return this.commentService.createComment(userId, createCommentDto);
+  }
+
+  @Get(':id')
+  getPostById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) commentId: number) {
+    return this.commentService.getCommentById(userId, commentId)
+  }
+
+  @Delete(':id')
+  deleteCommentById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) commentId: number) {
+    return this.commentService.deleteCommentById(userId, commentId)
   }
 }
--- apps/comment/src/comment.service.ts
+++ apps/comment/src/comment.service.ts
@@ -1,5 +1,5 @@
 import { PrismaService } from '@app/comman/prisma/prisma.service';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
@@ -39,5 +39,51 @@ export class CommentService {
     } catch (error) {
       throw error;
     }
+  }
+
+  async getCommentById(userId: number, commentId: number) {
+    try {
+      const user = await this.prisma.user.findUnique({ where: { id: userId } });
+      const comment = await this.prisma.comment.findUnique({ where: { id: commentId } });
+  
+      if (!user) {
+        throw new NotFoundException(`User with ID ${userId} not found`);
+      }
+  
+      if (!comment) {
+        throw new NotFoundException(`Comment with ID ${commentId} not found`);
+      }
+
+      const commentOnPost = await this.prisma.comment.findFirst({
+        where: {
+          id: commentId,
+        },
+        include: {
+          user: {
+            select: {
+              name: true,
+              profileImage: true,
+            }
+          },
+          post: true,
+        },
+      })
+      return commentOnPost
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async deleteCommentById(userId: number, commentId: number) {
+      const comment = await this.prisma.comment.findUnique({ where: { id: commentId } });
+      if (!comment || comment.userId!== userId) {
+        throw new ForbiddenException(`Access denied`);
+      }
+      await this.prisma.comment.delete({
+        where: {
+          id: commentId,
+        },
+      });
+      return { message: `Comment with ID ${commentId} deleted` };
   }
 }
