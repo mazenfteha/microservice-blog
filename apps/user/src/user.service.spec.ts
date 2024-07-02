304cce584b7eca9f8ecf4e18300488ea9f35e840
emadhashem <eali32028@gmail.com>
2024-07-02 22:49:18 +0300

    user service full tested

--- apps/user/src/user.service.spec.ts
+++ apps/user/src/user.service.spec.ts
@@ -14,18 +14,99 @@ const user = {
   password: '12345678',
 };
 
-// const EMAIL_ERROR = 'xxxxxxxxxxxxxxxx';
-
 const db = {
   user: {
-    create: jest.fn().mockResolvedValue(user),
-    findUnique: jest.fn().mockResolvedValue(user),
+    create: jest
+      .fn()
+      .mockResolvedValueOnce(user)
+      .mockRejectedValueOnce(() => {
+        throw new Error();
+      }),
+    findUnique: jest
+      .fn()
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(user)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValue(user),
+    update: jest.fn().mockResolvedValue({
+      ...user,
+      email: 'new@tst.com',
+      name: 'test1',
+      profileImage: 'image_url',
+    }),
+  },
+  follow: {
+    findUnique: jest
+      .fn()
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce({
+        id: 1,
+        followerId: 1,
+        followingId: 2,
+      })
+      .mockResolvedValueOnce({
+        id: 1,
+        followerId: 1,
+        followingId: 2,
+      })
+      .mockResolvedValueOnce(null),
+    create: jest.fn().mockResolvedValue({
+      id: 1,
+      followerId: 1,
+      followingId: 2,
+    }),
+    delete: jest.fn().mockResolvedValue(null),
+    findMany: jest
+      .fn()
+      .mockResolvedValueOnce([
+        {
+          id: 1,
+          follower: {
+            name: 'test',
+            profileImage: 'image',
+          },
+        },
+      ])
+      .mockResolvedValue([
+        {
+          id: 1,
+          following: {
+            name: 'test',
+            profileImage: 'image',
+          },
+        },
+      ]),
+  },
+  notification: {
+    create: jest.fn().mockResolvedValue(null),
   },
 };
 
 const argon = {
   hash: jest.fn().mockResolvedValue('hash'),
-  verify: jest.fn().mockResolvedValue(true),
+  verify: jest.fn().mockResolvedValueOnce(true).mockResolvedValue(false),
 };
 
 const jwt = {
@@ -35,7 +116,8 @@ const jwt = {
 describe('UserService', () => {
   let userService: UserService;
   let prisma: PrismaService;
-  beforeAll(async () => {
+  let argonService: ArgonService;
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
@@ -62,13 +144,22 @@ describe('UserService', () => {
         },
         {
           provide: CloudinaryService,
-          useValue: {},
+          useValue: {
+            uploadImage: jest.fn().mockResolvedValue({
+              url: 'https://example.com/image.jpg',
+            }),
+          },
         },
       ],
     }).compile();
 
     userService = module.get<UserService>(UserService);
     prisma = module.get<PrismaService>(PrismaService);
+    argonService = module.get<ArgonService>(ArgonService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('Should be Defined', async () => {
@@ -88,6 +179,16 @@ describe('UserService', () => {
       expect(result).toHaveProperty('email');
       expect(result.name).toMatch(dto.name);
     });
+
+    it('should throw error if user already exists', async () => {
+      const dto: SignupDto = {
+        name: 'test',
+        email: 'XXXXXXXXXXXX',
+        password: '12345678',
+      };
+
+      await expect(userService.signup(dto)).rejects.toThrow();
+    });
   });
 
   describe('sign in', () => {
@@ -100,83 +201,214 @@ describe('UserService', () => {
       expect(result).toHaveProperty('access_token');
       expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
     });
-  });
-});
 
-const dbException = {
-  user: {
-    findUnique: jest.fn().mockResolvedValue(null),
-    create: jest.fn().mockImplementation(() => {
-      throw new Error();
-    }),
-  },
-};
-
-describe('user service handling exception ', () => {
-  let userService: UserService;
-  let prisma: PrismaService;
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UserService,
-        {
-          provide: ArgonService,
-          useValue: argon,
-        },
-        { provide: JwtService, useValue: jwt },
-        {
-          provide: PrismaService,
-          useValue: dbException,
-        },
-        {
-          provide: ConfigService,
-          useValue: {
-            get: jest.fn().mockResolvedValue('secret'),
-          },
-        },
-        {
-          provide: RabbitMQService,
-          useValue: {
-            sendMessage: jest.fn(),
-          },
-        },
-        {
-          provide: CloudinaryService,
-          useValue: {},
-        },
-      ],
-    }).compile();
-
-    userService = module.get<UserService>(UserService);
-    prisma = module.get<PrismaService>(PrismaService);
-  });
-
-  it('should services to be defined', () => {
-    expect(userService).toBeDefined();
-    expect(prisma).toBeDefined();
-  });
-
-  describe('sign up with error', () => {
-    it('should throw error if email already exist', async () => {
-      const dto: SignupDto = {
-        name: 'test',
-        email: 'XXXXXXXXXXXX',
-        password: '12345678',
-      };
-
-      await expect(userService.signup(dto)).rejects.toThrow();
-      expect(prisma.user.create).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('sign in with error', () => {
-    it('should throw error if email does not exist', async () => {
+    it('should throw error if user not found', async () => {
       const dto = {
         email: 'XXXXXXXXXXXX',
         password: '12345678',
       };
+      await expect(userService.signin(dto)).rejects.toThrow();
+    });
+
+    it('should throw error if password is incorrect', async () => {
+      const dto = {
+        email: 'XXXXXXXXXXX',
+        password: 'XXXXXXXXXXXX',
+      };
 
       await expect(userService.signin(dto)).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+      expect(argonService.verify).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('editUser', () => {
+    it('should edit user', async () => {
+      const dto = {
+        name: 'test1',
+        email: 'new@tst.com',
+      };
+      const result = await userService.editUser(1, dto);
+      expect(result).toHaveProperty('name');
+      expect(result.name).toMatch(dto.name);
+      expect(result.email).toMatch(dto.email);
+    });
+  });
+
+  describe('updateUserProfileImage', () => {
+    it('should update user profile image', async () => {
+      const userId = 1;
+      const updatedUser = await userService.updateUserProfileImage(
+        userId,
+        new Buffer(''),
+      );
+      expect(updatedUser).toHaveProperty('profileImage');
+      expect(updatedUser.profileImage).toBeTruthy();
+      expect(updatedUser).not.toHaveProperty('password');
+    });
+
+    it('should throw error if user not found', async () => {
+      const userId = 1;
+      await expect(
+        userService.updateUserProfileImage(userId, new Buffer('')),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('deleteUserProfileImage', () => {
+    it('should delete user profile image', async () => {
+      const userId = 1;
+      const result = await userService.deleteUserProfileImage(userId);
+      expect(result).toHaveProperty('message');
+    });
+
+    it('should throw error if user not found', async () => {
+      const userId = 1;
+      await expect(
+        userService.deleteUserProfileImage(userId),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('followUser', () => {
+    it('should follow user', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      const follow = {
+        id: 1,
+        followerId: 1,
+        followingId: 2,
+      };
+      const result = await userService.createFollow(followerId, {
+        followerId,
+        followingId,
+      });
+      expect(result).toHaveProperty('followerId');
+      expect(result).toHaveProperty('followingId');
+      expect(result).toHaveProperty('id');
+      expect(result.id).toBe(follow.id);
+    });
+
+    it('should throw error if follower not found', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.createFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+      expect(prisma.follow.findUnique).not.toHaveBeenCalled();
+    });
+
+    it('should throw error if following not found', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.createFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(2);
+      expect(prisma.follow.findUnique).not.toHaveBeenCalled();
+    });
+
+    it('should throw error if follow relationship already exists', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.createFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(2);
+      expect(prisma.follow.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('un follow User', () => {
+    it('should un follow user', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await userService.deleteFollow(followerId, {
+        followerId,
+        followingId,
+      });
+    });
+
+    it('should throw if follower not found', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.deleteFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw if following not found', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.deleteFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(2);
+    });
+
+    it('should throw if there is no follow', async () => {
+      const followerId = 1;
+      const followingId = 2;
+      await expect(
+        userService.deleteFollow(followerId, {
+          followerId,
+          followingId,
+        }),
+      ).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(2);
+      expect(prisma.follow.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('get my followers', () => {
+    it('should return my followers', async () => {
+      const userId = 1;
+      const result = await userService.getFollowers(userId);
+      result.forEach((follower) => {
+        expect(follower).toHaveProperty('username');
+        expect(follower).toHaveProperty('profilePicture');
+      });
+    });
+
+    it('should throw if user not found', async () => {
+      const userId = 1;
+      await expect(userService.getFollowers(userId)).rejects.toThrow();
+      expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('get my followings', () => {
+    it('should return my followings', async () => {
+      const userId = 1;
+      const result = await userService.getFollowing(userId);
+      result.forEach((follower) => {
+        expect(follower).toHaveProperty('username');
+        expect(follower).toHaveProperty('profilePicture');
+      });
+    });
+
+    it('should throw if user not found', async () => {
+      const userId = 1;
+      await expect(userService.getFollowing(userId)).rejects.toThrow();
       expect(prisma.user.findUnique).toHaveBeenCalledTimes(1);
     });
   });
